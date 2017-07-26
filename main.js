d046429b35b8f5dcb60241483dd3c82916c98927
Kurogi <30377909+Kurogi1314@users.noreply.github.com>
2017-07-27 01:08:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import Discord form 'discord.js'
+
+const Client = new Discord.Client({
+   disableEveryone: true,
+  messageCacheMaxSize: 500,
+  messageCacheLifetime: 120,
+  messageSweepInterval: 60
+});
